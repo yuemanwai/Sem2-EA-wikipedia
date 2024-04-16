5c9d6d130dec08eaa2260bf312f4d1f652148786
YUE Man Wai <143982071+yuemanwai@users.noreply.github.com>
2024-04-16 16:16:43 +0000

    minor edits

--- app/config.py
+++ app/config.py
@@ -1,22 +1,24 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    # 放上cloud, 可以選擇用rds, 只需放endpoint係度
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or \
-        'postgresql://postgres:postgres@postgresdb:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    # 放上cloud, 可以簡用rds, 放endpoint係(.env)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['wai@example.com']
+    ADMINS = os.environ.get('ADMINS').split(',')
     POSTS_PER_PAGE = 10
     LANGUAGES = ['en', 'es', 'zh']
     # 要有domain,然後去google申請條key, https://www.google.com/recaptcha/admin
-    RECAPTCHA_PUBLIC_KEY='no-key'
-    RECAPTCHA_PRIVATE_KEY='no-key'
+    RECAPTCHA_PUBLIC_KEY=os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY=os.environ.get('RECAPTCHA_PRIVATE_KEY')
